34747808688c7a0386cab786f488f1b1a0e6f281
BesokDev <74706532+BesokDev@users.noreply.github.com>
2022-08-25 13:50:27 +0200

    create show employes action

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Employe;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +11,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'default_home')]
-    public function home(): Response
+    public function home(EntityManagerInterface $entityManager): Response
     {
-        return $this->render('default/home.html.twig');
+        # Cette instruction nous permet de récupérer en BDD toutes les lignes de la table "employe".
+        # Ceci est possible grâce au Repository, accessible par $entityManager.
+        $employes = $entityManager->getRepository(Employe::class)->findAll();
+
+        # Nous devons maintenant passer la variable $employes (qui contient tous les employés de la BDD)
+        # à notre vue Twig, pour pouvoir afficher les différentes données.
+        return $this->render('default/home.html.twig', [
+            'employes' => $employes
+        ]);
     }
 }
